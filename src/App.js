2870be183f29e49d609246aed5b528596fd1ca0c
Tom <tchao770@gmail.com>
2021-02-25 14:02:54 -0800

    Hashrouter

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Header from "./components/Header";
 import './App.scss';
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter, Switch, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Mortgage from "./pages/Mortgage";
 import RealEstate from "./pages/RealEstate";
@@ -11,7 +11,7 @@ import Footer from "./components/Footer";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <HashRouter>
         <Header />
         <Switch>
           <Route path="/Nestegg/home" component={Home} />
@@ -20,7 +20,7 @@ function App() {
           <Route path="/Nestegg/about" component={About}/>
           <Route path="/Nestegg/contact" component={Contact} />
         </Switch>
-      </Router>
+      </HashRouter>
       <Footer />
     </div>
   );
