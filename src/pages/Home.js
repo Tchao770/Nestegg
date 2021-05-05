7a560402039c0967d03b9b3b5c005faba284d8ba
Tom <tchao770@gmail.com>
2021-05-04 23:38:09 -0700

    Made font sizes consistent across

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,23 +11,23 @@ function Home({ match }) {
 				<div className="HomePage">
 					<section className="AboutSection">
 						<div className="QuoteLeft">
-						<h3><i>Home is more than just where you live.</i></h3>
-						<span>
-							Home can be a place to thrive, grow and build a lifetime investment. We work to make our clients’ best version of home a reality.
+							<h3><i>Home is more than just where you live.</i></h3>
+							<span>
+								Home can be a place to thrive, grow and build a lifetime investment. We work to make our clients’ best version of home a reality.
 						</span>
 						</div>
 						<hr />
 						<div className="QuoteLeft">
-						<h3><i>Expert advice at every step.</i></h3>
-						<span>
-							Homeownership can be a maze of confusing terminology, processes and requirements that can intimidate even seasoned investors. We untangle the mess and help you make a plan based on your goals.
+							<h3><i>Expert advice at every step.</i></h3>
+							<span>
+								Homeownership can be a maze of confusing terminology, processes and requirements that can intimidate even seasoned investors. We untangle the mess and help you make a plan based on your goals.
 						</span>
 						</div>
 						<hr />
 						<div className="QuoteLeft">
-						<h3><i>Everything is right here.</i></h3>
-						<span>
-							We handle real estate financing and real estate sales in-house. You don’t have to use both services, but when you do, you have a single point of contact handling everything in your transaction. Either way, you always get personalized service and peace of mind.
+							<h3><i>Everything is right here.</i></h3>
+							<span>
+								We handle real estate financing and real estate sales in-house. You don’t have to use both services, but when you do, you have a single point of contact handling everything in your transaction. Either way, you always get personalized service and peace of mind.
 						</span>
 						</div>
 					</section>
--- src/pages/RealEstate.js
+++ src/pages/RealEstate.js
@@ -17,19 +17,29 @@ function RealEstate() {
     return (
         <div className="PageStyles">
             <PhotoHeading src={realestateImg} name="real-estate-image" />
+            <h2 className="Quote">Your home is your most valuable asset. Whether you’re a first time buyer or a seasoned investor, we work to realize your goals of homeownership.</h2>
             <div className="RealEstatePage">
-                <h2 className="Quote">Your home is your most valuable asset. Whether you’re a first time buyer or a seasoned investor, we work to realize your goals of homeownership.</h2>
-                <section>
-                    <p>Finding your own little slice of California living doesn’t have to be daunting. We provide expert guidance to avoid getting stuck in a maze of endless home searches and bidding matches. Navigating the most competitive market in real estate is easier with consistent, personalized service from the beginning to the close of escrow and beyond.</p>
-                    <br />
-                    <p>Selling your home doesn’t have to be stressful. We work with you to tailor a strategy that maximizes your net return and suits your needs. You’ll always be well-informed and aware of every step of the process. Your best interest is always our top priority not just in selling your home for the best price but also taking necessary steps to minimize liability and diligently work with buyers toward a smooth and successful sale.</p>
-                    <br />
-                </section>
+                <div className="Content">
+                    <div className="QuoteLeft">
+                        <h3><i>Finding your own little slice of California living doesn’t have to be daunting.</i></h3>
+                        <span>
+                            We provide expert guidance to avoid getting stuck in a maze of endless home searches and bidding matches. Navigating the most competitive market in real estate is easier with consistent, personalized service from the beginning to the close of escrow and beyond.
+						</span>
+                    </div>
+                    <hr />
+                    <div className="QuoteLeft">
+                        <h3><i>Selling your home doesn’t have to be stressful.</i></h3>
+                        <span>
+                            We work with you to tailor a strategy that maximizes your net return and suits your needs. You’ll always be well-informed and aware of every step of the process. Your best interest is always our top priority not just in selling your home for the best price but also taking necessary steps to minimize liability and diligently work with buyers toward a smooth and successful sale.
+						</span>
+                    </div>
+                </div>
+
                 <Link to="/Nestegg/contact" className="EstateContactLink">
                     <button className="ContactButton">TALK TO US!</button>
                 </Link>
             </div>
-        </div>
+        </div >
     )
 }
 
