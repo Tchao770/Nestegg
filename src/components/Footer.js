06f155fc0de09cf9bb19f30121b6181885f8cb58
Tom <tchao770@gmail.com>
2021-05-12 19:10:06 -0700

    Adjusted footer spacing

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,17 @@ import lenderLogo from "../assets/lenderLogo.png";
 function Footer() {
     return (
         <footer className="FooterClass">
-            <img src={footerLogo} alt="nestegg-logo" />
+            <div className="FooterImg">
+                <img src={footerLogo} alt="nestegg-logo" />
+            </div>
             <p>
                 Nestegg Inc. - 12437 Lewis Street, Suite 206, Garden Grove, CA 92840<br />
                 info@nestegginc.net - Cal BRE # 01521541 - NMLS # 301815
             </p>
-            <img src={lenderLogo} alt="Equal Housing Lender" />
-        </footer>
+            <div className="FooterImg">
+                <img src={lenderLogo} alt="Equal Housing Lender" />
+            </div>
+        </footer >
     );
 }
 export default Footer;
